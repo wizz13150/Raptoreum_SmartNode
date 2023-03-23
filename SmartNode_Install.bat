2561d398e30e30514c60cee91b9443055f848988
Wizz_^ <lewizard@hotmail.fr>
2023-03-23 15:12:13 +0100

    Combine 2 launchers into one
    Combine 2 launchers into one

--- SmartNode_Install.bat
+++ SmartNode_Install.bat
@@ -1,16 +1,32 @@
 @echo off
 
-set DOWNLOAD_URL=https://raw.githubusercontent.com/wizz13150/Raptoreum_SmartNode/main/install.ps1
+echo Choose an option:
+echo [1] Install a Mainnet Smartnode
+echo [2] Install a Testnet Smartnode
+echo Enter your choice (1 or 2):
+set /p choice=
+
+if %choice%==1 (
+    set DOWNLOAD_URL=https://raw.githubusercontent.com/wizz13150/Raptoreum_SmartNode/main/install.ps1
+    set FILE_NAME=install.ps1
+) else if %choice%==2 (
+    set DOWNLOAD_URL=https://raw.githubusercontent.com/wizz13150/Raptoreum_SmartNode/main/install_testnet.ps1
+    set FILE_NAME=install_testnet.ps1
+) else (
+    echo Invalid choice. Please enter 1 or 2.
+    goto :eof
+)
+
 set USER_PROFILE=%USERPROFILE%
-set DOWNLOAD_PATH=%USER_PROFILE%\install.ps1
+set DOWNLOAD_PATH=%USER_PROFILE%\%FILE_NAME%
 
 echo Downloading file from %DOWNLOAD_URL%...
 powershell.exe -Command "& { Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -UseBasicParsing -OutFile '%DOWNLOAD_PATH%' }"
 
 if exist "%DOWNLOAD_PATH%" (
-  	echo File downloaded successfully!
-  	echo Running install.ps1 as Admin...
-	powershell.exe -ExecutionPolicy RemoteSigned -Command "Set-Content -Path '%DOWNLOAD_PATH%' -Value (Get-Content -Path '%DOWNLOAD_PATH%' -Encoding utf8) -Encoding utf8; Start-Process cmd.exe -ArgumentList '/c powershell.exe -ExecutionPolicy Bypass -File "%DOWNLOAD_PATH%"' -Verb RunAs
+    echo File downloaded successfully!
+    echo Running %FILE_NAME% as Admin...
+    powershell.exe -ExecutionPolicy RemoteSigned -Command "Set-Content -Path '%DOWNLOAD_PATH%' -Value (Get-Content -Path '%DOWNLOAD_PATH%' -Encoding utf8) -Encoding utf8; Start-Process cmd.exe -ArgumentList '/c powershell.exe -ExecutionPolicy Bypass -File "%DOWNLOAD_PATH%"' -Verb RunAs
 ) else (
-  echo Failed to download file.
+    echo Failed to download file.
 )
