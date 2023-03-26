316419dae1f336e8c355959e3ca1fda91ebe9e3c
Wizz_^ <lewizard@hotmail.fr>
2023-03-26 19:02:10 +0200

    Update SmartNode_Install.bat

--- SmartNode_Install.bat
+++ SmartNode_Install.bat
@@ -1,10 +1,23 @@
 @echo off
-
-echo Choose an option:
+echo.
+echo  =================================================================
+echo  ^|  (       *             (               )     )  (             ^|
+echo  ^|  )\ )  (  `     (      )\ )  *   )  ( /(  ( /(  )\ )          ^|
+echo  ^| (()/(  )\))(    )\    (()/(` )  /(  )\()) )\())(()/(   (      ^|
+echo  ^|  /(_))((_)()\((((_)(   /(_))( )(_))((_)\ ((_)\  /(_))  )\     ^|
+echo  ^| (_))  (_()((_))\ _ )\ (_)) (_(_())  _((_)  ((_)(_))_  ((_)    ^|
+echo  ^| / __^| ^|  \/  ^|(_)_\(_)^| _ \^|_   _^| ^| \^| ^| / _ \ ^|   \ ^| __^|   ^|
+echo  ^| \__ \ ^| ^|\/^| ^| / _ \  ^|   /  ^| ^|   ^| .` ^|^| (_) ^|^| ^|) ^|^| _^|    ^|
+echo  ^| ^|___/ ^|_^|  ^|_^|/_/ \_\ ^|_^|_\  ^|_^|   ^|_^|\_^| \___/ ^|___/ ^|___^|   ^|
+echo  ^|                                                               ^|
+echo  =================================================================
+echo.
 echo [1] Install a Mainnet Smartnode
 echo [2] Install a Testnet Smartnode
+echo.
 echo Enter your choice (1 or 2):
 set /p choice=
+
 
 if %choice%==1 (
     set DOWNLOAD_URL=https://raw.githubusercontent.com/wizz13150/Raptoreum_SmartNode/main/install.ps1
@@ -20,13 +33,18 @@ if %choice%==1 (
 set USER_PROFILE=%USERPROFILE%
 set DOWNLOAD_PATH=%USER_PROFILE%\%FILE_NAME%
 
+echo  =================================================================
 echo Downloading file from %DOWNLOAD_URL%...
 powershell.exe -Command "& { Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -UseBasicParsing -OutFile '%DOWNLOAD_PATH%' }"
 
 if exist "%DOWNLOAD_PATH%" (
+    echo.
     echo File downloaded successfully!
+    echo  =================================================================
     echo Running %FILE_NAME% as Admin...
     powershell.exe -ExecutionPolicy RemoteSigned -Command "Set-Content -Path '%DOWNLOAD_PATH%' -Value (Get-Content -Path '%DOWNLOAD_PATH%' -Encoding utf8) -Encoding utf8; Start-Process cmd.exe -ArgumentList '/c powershell.exe -ExecutionPolicy Bypass -File "%DOWNLOAD_PATH%"' -Verb RunAs
 ) else (
+    echo  =================================================================
     echo Failed to download file.
 )
+echo  =================================================================
