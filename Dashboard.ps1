d8898d7f7114c8e45084a6b7d00216ae4ee9c60d
Wizz_^ <lewizard@hotmail.fr>
2023-03-27 03:40:13 +0200

    Update Dashboard.ps1

--- Dashboard.ps1
+++ Dashboard.ps1
@@ -19,10 +19,7 @@ $smartnodeStatus = cmd /C "$raptoreumcli smartnode status" | ConvertFrom-Json
 Clear-Host
 # Display informations
 Write-Host -NoNewline "`r----------------------------------" -ForegroundColor Cyan
-Write-Host "`nRaptoreum Dashboard Pro 9000 Plus Supreme" -ForegroundColor Yellow
-Write-Host "`nMega Championship Edition Collector Deluxe" -ForegroundColor Yellow
-Write-Host "`nHyper Turbo Interstellar Ultimate Galaxy" -ForegroundColor Yellow
-Write-Host "`nGrandmaster Battlestation of the Multiverse" -ForegroundColor Yellow
+Write-Host "`nRaptoreum Dashboard Pro 9000 Plus" -ForegroundColor Yellow
 Write-Host "`r----------------------------------" -ForegroundColor Cyan
 Write-Host "`rChain........................: $($networkInfo.chain)" -ForegroundColor Green
 Write-Host "`rLocal block height...........: $blockHeight" -ForegroundColor Green
@@ -37,13 +34,14 @@ Write-Host "`rTransactions in mempool......: $($mempoolInfo.size)" -ForegroundCo
 Write-Host "`rMempool size in Mb...........: $([math]::Round($mempoolInfo.bytes / 1MB, 3)) Mb" -ForegroundColor Green
 Write-Host "`r----------------------------------" -ForegroundColor Cyan
 Write-Host "`rSmartnode status.............: $($smartnodeStatus.status)" -ForegroundColor Green
+Write-Host "`rEstimated reward per day.....: ..." -ForegroundColor Green
 Write-Host "`rIP address and port..........: $($smartnodeStatus.service)" -ForegroundColor Green
 Write-Host "`rSmartnode ProTX..............: $($smartnodeStatus.proTxHash)" -ForegroundColor Green
 Write-Host "`rSmartnode BLS Key............: ..." -ForegroundColor Green
 Write-Host "`rPayout address...............: $($smartnodeStatus.dmnState.payoutAddress)" -ForegroundColor Green
 Write-Host "`rRegistered height............: $($smartnodeStatus.dmnState.registeredHeight)" -ForegroundColor Green
 Write-Host "`rCurrent PoSe score...........: $($smartnodeStatus.dmnState.PoSePenalty)" -ForegroundColor Green
-Write-Host "`rTime to PoSe Score 0.........: $(($smartnodeStatus.dmnState.PoSePenalty) * 4) min" -ForegroundColor Green
+Write-Host "`rTime to PoSe Score 0.........: $([math]::Floor((($smartnodeStatus.dmnState.PoSePenalty) * 4) / 60))h $(($smartnodeStatus.dmnState.PoSePenalty) * 4 % 60)min" -ForegroundColor Green
 Write-Host "`rPoSe ban height..............: $($smartnodeStatus.dmnState.PoSeBanHeight)" -ForegroundColor Green
 Write-Host "`rLast revived height..........: $($smartnodeStatus.dmnState.PoSeRevivedHeight)" -ForegroundColor Green
 Write-Host "`r----------------------------------" -ForegroundColor Cyan
